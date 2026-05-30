66161a830efeb25ee91ee6f3ed4180aaf9ca6562
sandip8659 <vickyswain001122@gmail.com>
2026-05-30 12:55:20 +0530

    Add files via upload

---
+++ graterno.py
@@ -0,0 +1,8 @@
+a=int(input("enter the first number "))
+b=int(input("enter 2nd number "))
+if(a==b):
+    print("the numbers are equal")
+elif(a>b):
+    print("the first number is greater")
+else:
+    print("the second number is greater") 
